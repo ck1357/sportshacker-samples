33e9a01163b4de984e4b510f870bdff482de4dce
Justin Worrall <justin.worrall@gmail.com>
2013-10-06 07:40:54 +0100

    added python scripts

---
+++ simulate_season_points.py
@@ -0,0 +1,62 @@
+import lxml.html, json, re, urllib
+
+LivePricingUrl="http://livepricing.sportingindex.com/LivePricing.svc/jsonp/GetLivePricesByMeeting?meetingKey="
+
+def get_market_quotes(url):        
+    doc=lxml.html.fromstring(urllib.urlopen(url).read())
+    ids=dict([(li.attrib["key"], re.sub(" Points$", "", li.xpath("span[@class='markets']")[0].text))
+              for li in doc.xpath("//ul[@class='prices']/li")
+              if "key" in li.attrib])
+    quotes=json.loads(urllib.urlopen(LivePricingUrl+url.split("/")[-2]).read())
+    return [{"name": ids[quote["Key"]],
+             "so_far": tuple([int(tok) for tok in quote["SoFar"].split("/")]),
+             "bid": quote["Sell"],
+             "offer": quote["Buy"]}
+            for quote in quotes["Markets"]]
+
+def simulate_points(quotes, paths, draw_prob=0.3):
+    import random
+    simpoints=dict([(quote["name"], 
+                     [quote["so_far"][0] 
+                      for i in range(paths)])
+                    for quote in quotes])
+    ngames=2*(len(quotes)-1) 
+    for quote in quotes:
+        midprice=(quote["bid"]+quote["offer"])/float(2)
+        currentpoints, played = quote["so_far"]
+        toplay=ngames-played
+        expectedpoints=(midprice-currentpoints)/float(toplay) 
+        winprob=(expectedpoints-draw_prob)/float(3) 
+        for i in range(paths):
+            for j in range(toplay):
+                q=random.random()
+                if q < winprob:
+                    simpoints[quote["name"]][i]+=3
+                elif q < winprob+draw_prob:
+                    simpoints[quote["name"]][i]+=1
+    return [{"name": key,
+             "simulated_points": value}
+            for key, value in simpoints.items()]
+
+def calc_position_probabilities(simpoints, paths):
+    positionprob=dict([(team["name"], 
+                        [0 for i in range(len(simpoints))])
+                       for team in simpoints])
+    for i in range(paths):
+        sortedpoints=sorted([(team["name"], team["simulated_points"][i])
+                             for team in simpoints],
+                            key=lambda x: -x[-1])
+        for j in range(len(simpoints)):
+            name=sortedpoints[j][0]
+            positionprob[name][j]+=1/float(paths)
+    return [{"name": key,
+             "position_probabilities": value}
+            for key, value in positionprob.items()]
+
+def generate_position_probabilities(url, paths):
+    quotes=get_market_quotes(url)
+    simpoints=simulate_points(quotes, paths)
+    return calc_position_probabilities(simpoints, paths)
+
+if __name__=="__main__":
+    print generate_position_probabilities("http://www.sportingindex.com/spread-betting/football-domestic/premier-league/mm4.uk.meeting.4191659/premier-league-points-2013-2014", 10000)
---
+++ stan_james_prices.py
@@ -0,0 +1,96 @@
+from lxml import etree
+
+import urllib
+
+UrlPatterns={
+    "root": "http://www.stanjames.com/cache/boNavigationList/541/UK/%s.xml",
+    "group": "http://www.stanjames.com/cache/marketgroup/UK/%s.xml",
+    "event": "http://www.stanjames.com/cache/event/UK/%s.xml",
+    "market": "http://www.stanjames.com/cache/market/UK/%s.xml"
+    }
+
+def get_categories_for_sport(sport):    
+    url=UrlPatterns["root"] % sport["id"]
+    doc=etree.fromstring(urllib.urlopen(url).read())
+    return [{"name": el.xpath("name")[0].text,
+             "id": el.xpath("idfwbonavigation")[0].text}
+            for el in doc.xpath("//bonavigationnode")]
+
+def get_groups_for_category(category):
+    url=UrlPatterns["root"] % category["id"]
+    doc=etree.fromstring(urllib.urlopen(url).read())
+    return [{"name": el.xpath("name")[0].text,
+             "id": el.xpath("idfwmarketgroup")[0].text}
+            for el in doc.xpath("//marketgroup")]
+
+def get_events_for_group(group):
+    url=UrlPatterns["group"] % group["id"]
+    doc=etree.fromstring(urllib.urlopen(url).read())
+    return [{"name": el.xpath("eventname")[0].text,
+             "id": el.xpath("idfoevent")[0].text}
+            for el in doc.xpath("//market")]
+
+def get_markets_for_event(event):
+    url=UrlPatterns["event"] % event["id"]
+    doc=etree.fromstring(urllib.urlopen(url).read())
+    return [{"name": el.xpath("name")[0].text,
+             "id": el.xpath("idfomarket")[0].text}
+            for el in doc.xpath("//market")]
+
+def get_selections_for_market(market):
+    url=UrlPatterns["market"] % market["id"]
+    doc=etree.fromstring(urllib.urlopen(url).read())
+    return [{"name": el.xpath("name")[0].text,
+             "id": el.xpath("idfoselection")[0].text,
+             "price": "%s/%s" % (el.xpath("currentpriceup")[0].text,
+                                 el.xpath("currentpricedown")[0].text)}
+            for el in doc.xpath("//selection")]
+
+import re, time
+
+SportIds={"Football": "58974.2"}
+
+Handlers=[get_categories_for_sport,
+          get_groups_for_category,
+          get_events_for_group,
+          get_markets_for_event,
+          get_selections_for_market]
+
+def crawl_events(path, wait=1):
+    tokens=path.split("/")
+    if tokens[0] not in SportIds:
+        raise RuntimeError("Sport not found")
+    stack, results = [], []
+    stack.append(({"name": tokens[0],
+                   "id": SportIds[tokens[0]]}, 
+                  [tokens[0]],
+                  0))
+    while True:
+        if stack==[]:
+            break
+        head, stack = stack[0], stack[1:]
+        parent, path, depth = head
+        print "/".join(path)
+        handler=Handlers[depth]
+        if depth < len(tokens)-1:
+            stack+=[(result, 
+                     path+[result["name"]],
+                     depth+1)
+                    for result in handler(parent)
+                    if re.search(tokens[depth+1], result["name"])]
+        else:
+            for result in handler(parent):
+                result["path"]=path+[result["name"]]
+                results.append(result)
+        time.sleep(wait)
+    return results
+
+if __name__=="__main__":
+    try:
+        results=crawl_events("Football/^English Premier League$/Matches/ v /Match Betting")
+        for result in results:
+            print ("%s/%s" % (result["path"][-3], 
+                              result["path"][-1]),
+                   result["price"])
+    except RuntimeError, error:
+        print "Error: %s" % str(error)
