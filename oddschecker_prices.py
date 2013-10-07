d2bd928b7cb2a53de9ef2cccd0684e13e81b5145
Justin Worrall <justin.worrall@gmail.com>
2013-10-07 11:36:21 +0100

    added oddschecker prices

--- oddschecker_prices.py
+++ oddschecker_prices.py
@@ -1,6 +1,6 @@
 import lxml.html, re, urllib
 
-def parse_quote(text):
+def parse_fractional_quote(text):
     tokens=[int(tok) for tok in text.split("/")]
     if len(tokens)==1:
         tokens.append(1)
@@ -10,26 +10,30 @@ def get_prices(url):
     doc=lxml.html.fromstring(urllib.urlopen(url).read())
     table=doc.xpath("//table[@class='eventTable ']")[0]
     rows=table.xpath("tbody/tr")
-    selections=[]
+    items={}
     for row in rows:
         oddscheckerid=row.attrib["data-participant-id"]
-        selectionname=None
+        name=None
         for td in row.xpath("td"):
             if not "id" in td.attrib:
                 continue
             suffix=td.attrib["id"].split("_")[-1]
             if suffix=="name":
-                selectionname=td.text
+                name=td.text
                 continue
             if (suffix=="best" or
                 td.text in [None, "SP"] or
                 re.search("^\\d+\\-\\d+$", td.text)):
                 continue
-            selection={"name": selectionname,
-                       "bookmaker": suffix,
-                       "price": parse_quote(td.text)}
-            selections.append(selection)
-    return selections
+            items.setdefault(name, {"name": name,
+                                    "bookmaker": None,
+                                    "price": 1e10})
+            price=parse_fractional_quote(td.text)
+            if price < items[name]["price"]:
+                items[name]["price"]=price
+                items[name]["bookmaker"]=suffix
+    return sorted(items.values(),
+                  key=lambda x: -x["price"])
 
 if __name__=="__main__":
-    print get_prices("http://www.oddschecker.com/football/english/premier-league/winner")
+    print get_prices("http://www.oddschecker.com/football/english/premier-league/top-3-finish")
